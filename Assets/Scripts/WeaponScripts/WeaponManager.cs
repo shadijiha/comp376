c5fe205f27a4360e14fed1e602cda2959112d5de
DWU729 <donovano@email.com>
2022-11-09 21:18:57 -0500

    Updated weapon positioning so the weapons are visible in first person.

--- Assets/Scripts/WeaponScripts/WeaponManager.cs
+++ Assets/Scripts/WeaponScripts/WeaponManager.cs
@@ -84,7 +84,9 @@ public class WeaponManager : NetworkBehaviour
     private void Equip(PlayerWeapon weapon)
     {
         //weaponHolder.position, weaponHolder.rotation
-        weapon.model = (GameObject)Instantiate(msWeaponArr[(int)weapon.weaponType]);
+        weapon.model = (GameObject)Instantiate(
+            msWeaponArr[(int)weapon.weaponType]);
+        weapon.model.transform.position = weaponHolder.transform.position;
         weapon.model.transform.SetParent(weaponHolder);
 
         mCurrentGraphics = weapon.model.GetComponentInChildren<WeaponGraphics>();
