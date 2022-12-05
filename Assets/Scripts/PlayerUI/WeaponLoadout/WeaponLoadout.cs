ab6486ddaa42950233d176c97ffff31313ae172e
Timmy-Pierre Keo <timmypierre.keo@gmail.com>
2022-12-05 17:22:12 -0500

    Get weapon types

---
+++ Assets/Scripts/PlayerUI/WeaponLoadout/WeaponLoadout.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static PlayerWeapon;
+
+public class WeaponLoadout : MonoBehaviour
+{
+    [SerializeField] private List<WeaponType> primaryWeaponTypes;
+    [SerializeField] private List<WeaponType> secondaryWeaponTypes;
+
+    private List<PlayerWeapon> primaryWeapons;
+    private List<PlayerWeapon> secondaryWeapons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
