aea5be4959ec44d27e740a08cab111f8bd0a790a
BozhidarLeshev <52761503+BozhidarLeshev@users.noreply.github.com>
2022-12-07 17:16:06 -0500

    Add files via upload

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+/**
+ * NOTE: Ya de l'asti de sauce qui marche pas avec les layers!!!!
+ */
+[RequireComponent(typeof(WeaponManager))]
+public class PlayerShoot : NetworkBehaviour
+{
+    public const string PLAYER_TAG = "Player";
+
+
+                        private PlayerWeapon    m_CurrentWeapon;
+                        private PlayerControler m_controler;
+                        private PlayerMotor     m_motor;
+                        private Vector3         m_previousPosition;
+                        public  WeaponManager   m_WeaponManager;
+    [SerializeField]    public  RectTransform   crosshair;
+    [SerializeField]    public  Camera          cam;
+    [SerializeField]    public  Camera          weaponCam;
+    [SerializeField]    public  LayerMask       mask;
+    [SerializeField]    public  CameraRecoil    cameraRecoil;
+    [SerializeField]    public  ModelRecoil     modelRecoil;
+    [SerializeField]    public  AudioSource     weaponSound;
+                        public  float           m_fMovement;
+
+    private bool    m_bMoving; 
+
+    public GameObject laserShot;
+    //public GameObject laserShotTarget;
+    public GameObject weaponRotation;
+
+
+    Ray ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cam == null) {
+            this.enabled = false;
+            throw new Exception("PlayerShoot: No camera attached");
+        }
+
+        // Make the gun in the gun layer for the purpose of the weapon camera
+        // The weapon camera is responsible of preventing weapon cliping
+        m_WeaponManager = GetComponent<WeaponManager>();
+        m_controler     = GetComponent<PlayerControler>();
+        m_motor         = GetComponent<PlayerMotor>();
+
+        m_CurrentWeapon = m_WeaponManager.GetCurrentWeapon();
+
+        // Load the weapon so the player doesn't need to reload before first using it.
+        m_CurrentWeapon.currentLoadedAmmo   = m_CurrentWeapon.magazineSize;
+        m_CurrentWeapon.currentSpareAmmo    = m_CurrentWeapon.maxAmmo;
+        m_CurrentWeapon.readyToShoot        = true;
+
+
+        //mask &= LayerMask.NameToLayer("RemotePlayerLayer");
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+
+        if (crosshair == null)
+        {
+            crosshair               = GameObject.FindObjectOfType<PlayerSetup>().playerUIInstance.GetComponentInChildren<DynamicCrosshair>().GetComponent<RectTransform>();
+        }
+
+        if (m_CurrentWeapon.scope == null)
+        {
+            m_CurrentWeapon.scope   = GameObject.FindObjectOfType<PlayerSetup>().playerUIInstance.GetComponentInChildren<ScopeOverlay>().GetComponent<Image>();
+        }
+
+        m_CurrentWeapon = m_WeaponManager.GetCurrentWeapon();
+
+        // Unify both methods of shooting (semi/full-auto) under one variable
+        if (m_CurrentWeapon.allowContinuousFire)
+        {
+            m_CurrentWeapon.shooting = Input.GetButton("Fire1") || Input.GetButtonDown("Fire1");
+        }
+        else
+        {
+            m_CurrentWeapon.shooting = Input.GetButtonDown("Fire1");
+        }
+
+        //Alt Fire
+        if (Input.GetButton("Fire2"))
+        {
+            m_CurrentWeapon.AltFireActivate(this);
+        }
+        else
+        {
+            m_CurrentWeapon.AltFireDeactivate(this);
+        }
+
+        // Update player movement speed.
+        m_controler.SetSpeedMultiplier(m_CurrentWeapon.speedMultiplier);
+
+        // Reload
+        if (Input.GetButtonDown("Reload") && 
+            m_CurrentWeapon.currentLoadedAmmo < m_CurrentWeapon.magazineSize && 
+            !(m_CurrentWeapon.currentSpareAmmo == 0)&&
+            m_CurrentWeapon.reloading == false)
+        {
+            Reload();
+        }
+
+        // Shoot
+        if (m_CurrentWeapon.readyToShoot            && 
+            m_CurrentWeapon.shooting                && 
+            !m_CurrentWeapon.reloading              && 
+            m_CurrentWeapon.currentLoadedAmmo > 0   &&
+            m_controler.getShootingBlock() == false)
+        {
+            Shoot();
+        }
+
+        // Update current movement speed
+        m_fMovement = Mathf.Lerp(m_fMovement, Mathf.Clamp(m_motor.currentVelocity * 10, 0f, 2f), 5f * Time.deltaTime);
+
+        // If not shooting, recover from spread
+        if (m_CurrentWeapon.readyToShoot)
+        { 
+            m_CurrentWeapon.currentSpread -= m_CurrentWeapon.spreadRecovery * Time.deltaTime * 60;
+            if (m_CurrentWeapon.currentSpread < m_CurrentWeapon.minSpread)
+            {
+                m_CurrentWeapon.currentSpread = m_CurrentWeapon.minSpread;
+            }
+        }
+
+        
+        UpdateCrosshair();
+    }
+
+    /// <summary>
+    /// Called on the server when the player shoots
+    /// </summary>
+    [Command]
+    public void CmdOnShoot() {
+        RpcDoShootEffect();
+    }
+
+    /// <summary>
+    /// Called on the server when we hit something
+    /// </summary>
+    /// <param name="pos">The hit point</param>
+    /// <param name="normal">The hit normal</param>
+    [Command]
+    public void CmdOnHit(Vector3 pos, Vector3 normal) {
+        RpcDoHitEffect(pos, normal);
+    }
+
+    
+    //[Command]
+    public void CmdOnHitLaser(Vector3 rayOrigine, Vector3 endPoint, float laserShotSpeed)
+    {
+        ShootLaser(rayOrigine, endPoint, laserShotSpeed);
+    }
+    
+
+    /// <summary>
+    /// Called on all clients when we need to do a
+    /// shoot effect
+    /// </summary>
+    [ClientRpc]
+    public void RpcDoShootEffect() {
+        ParticleSystem particle = Instantiate(
+                                        m_WeaponManager.GetCurrentWeaponGraphics().muzzelFlash, 
+                                        GetComponentInChildren<ParticleOrigin>().gameObject.transform);
+
+        Destroy(particle.gameObject, 0.5f);
+    }
+
+    /// <summary>
+    /// Shows the hit effect on all Clients
+    /// </summary>
+    /// <param name="pos">The hit point</param>
+    /// <param name="normal">The surface normal</param>
+    [ClientRpc]
+    public void RpcDoHitEffect(Vector3 pos, Vector3 normal) {
+        GameObject temp = (GameObject)Instantiate(m_WeaponManager.GetCurrentWeaponGraphics().hitEffectPrefab, pos, Quaternion.LookRotation(normal));
+        Destroy(temp, 2.0f);
+    }
+
+    /// <summary>
+    /// Sets the weapon to reloading and invokes the ReloadFinished function after the reload time is completed.
+    /// </summary>
+    [Client]
+    public void Reload()
+    {
+        if (m_CurrentWeapon.currentSpareAmmo == 0)
+        {
+            return;
+        }
+        else
+        {
+            //m_CurrentWeapon.reloading = true;
+
+            Vector3 originalPosition = m_CurrentWeapon.model.transform.localPosition;
+            StartCoroutine(ReloadAnim());
+            //To prevent the weapon getting permanently displaced in case of timing/calculation error
+            //m_CurrentWeapon.model.transform.localPosition = originalPosition;
+
+            Invoke("ReloadFinished",0f);
+            //Invoke("ReloadFinished", (m_CurrentWeapon.hasted ? m_CurrentWeapon.reloadTime * 0.5f : m_CurrentWeapon.reloadTime));
+
+        }
+    }
+
+    /// <summary>
+    /// Animates the reload of the weapon.
+    /// </summary>
+    [Client]
+    public IEnumerator ReloadAnim()
+    {
+        //translation
+        Vector3 originalPosition = m_CurrentWeapon.model.transform.localPosition;
+        Vector3 holdPosition = m_CurrentWeapon.model.GetComponentInChildren<HoldPt>().gameObject.transform.localPosition;
+        Vector3 moveDirection = m_CurrentWeapon.model.transform.localPosition - holdPosition;
+        moveDirection.Normalize();
+        float moveBy =  0.01f;
+
+        //rotation
+        Quaternion originalOrientation = m_CurrentWeapon.model.transform.localRotation;
+        Vector3 currentOrientation = m_CurrentWeapon.model.transform.localRotation.eulerAngles;
+        Vector3 rotationBy = new Vector3(0.25f, 0, 0);
+
+        float firstStageTimer = 0;
+        float secondStageTimer = 0;
+        while (firstStageTimer <= m_CurrentWeapon.reloadTime)
+        {
+            if (firstStageTimer <= m_CurrentWeapon.reloadTime / 2f)
+            {
+
+                currentOrientation = currentOrientation + rotationBy * (firstStageTimer + 1f);
+            
+                m_CurrentWeapon.model.transform.localPosition = m_CurrentWeapon.model.transform.localPosition - moveDirection*moveBy * firstStageTimer;
+                m_CurrentWeapon.model.transform.localRotation = Quaternion.Euler(currentOrientation);
+
+
+            }
+            else
+            {
+                currentOrientation = currentOrientation - rotationBy * (secondStageTimer + 1f);
+                m_CurrentWeapon.model.transform.localPosition = m_CurrentWeapon.model.transform.localPosition + moveDirection*moveBy * secondStageTimer;
+                m_CurrentWeapon.model.transform.localRotation = Quaternion.Euler(currentOrientation);
+                secondStageTimer += Time.deltaTime;
+            }
+            firstStageTimer += Time.deltaTime;
+            yield return null;
+        }
+        //To prevent the weapon getting permanently displaced in case of timing/calculation error
+        m_CurrentWeapon.model.transform.localPosition = originalPosition;
+        m_CurrentWeapon.model.transform.localRotation = originalOrientation;
+    }
+
+    /// <summary>
+    /// Reloads the weapon, subtracting from the spareAmmo and refilling the magazine.
+    /// </summary>
+    [Client]
+    public void ReloadFinished()
+    {
+        if (!m_CurrentWeapon.reloading)
+        {
+            Debug.Log("Reload Aborted.");
+        }
+        else
+        {
+
+            if (m_CurrentWeapon.currentSpareAmmo        >=  m_CurrentWeapon.magazineSize - m_CurrentWeapon.currentLoadedAmmo)
+            {
+                m_CurrentWeapon.currentSpareAmmo        -=  m_CurrentWeapon.magazineSize - m_CurrentWeapon.currentLoadedAmmo;
+                m_CurrentWeapon.currentLoadedAmmo       =   m_CurrentWeapon.magazineSize;
+            }
+            else if (m_CurrentWeapon.currentSpareAmmo   >   0)
+            {
+                // If there is not enough spare ammo to fully refill the magazine, partially reload it instead.
+                m_CurrentWeapon.currentLoadedAmmo       +=  m_CurrentWeapon.currentSpareAmmo;
+                m_CurrentWeapon.currentSpareAmmo        =   0; 
+            }
+            else
+            {
+                // Infinite Ammo Weapon
+                m_CurrentWeapon.currentLoadedAmmo       =   m_CurrentWeapon.magazineSize;
+            }
+
+            m_CurrentWeapon.reloading = false;
+        }
+    }
+    
+    
+    public void ShootLaser(Vector3 shotOriginPosition, Vector3 endPoint,  float laserShotSpeed)
+    {
+        GameObject shot = Instantiate(laserShot, shotOriginPosition, Quaternion.LookRotation(endPoint - shotOriginPosition));
+        //Quaternion.identity);
+        shot.GetComponent<Rigidbody>().velocity = (endPoint- shotOriginPosition) * laserShotSpeed;
+
+        GameObject.Destroy(shot, 2f);
+    }
+
+
+  
+
+    [Client]
+    public void Shoot()
+    {
+        m_CurrentWeapon.readyToShoot    = false;
+
+        (string, float) invokeDetails   = m_CurrentWeapon.Shoot(this);
+
+        Invoke(invokeDetails.Item1, invokeDetails.Item2);
+    }
+
+    public void UpdateCrosshair()
+    {
+        crosshair.sizeDelta     = new Vector2(  500 * (m_CurrentWeapon.currentSpread + m_CurrentWeapon.currentSpread * m_fMovement * m_CurrentWeapon.movementSpread),
+                                                500 * (m_CurrentWeapon.currentSpread + m_CurrentWeapon.currentSpread * m_fMovement * m_CurrentWeapon.movementSpread));
+    }
+
+    public void ReadyToShoot()
+    {
+        m_CurrentWeapon.readyToShoot = true;
+    }
+
+    [Command]
+    public void CmdPlayerShot(string hit_id, string src, int damage) {
+        // Do the damage stuff
+        Debug.Log(hit_id + " has been shot");
+
+        // In the future shoud pass the source to grant assists
+        Player p = GameManager.GetPlayer(hit_id);
+        p.RpcTakeDamage(damage, src);
+
+        Debug.Log(hit_id + " has " + p.GetHealth());
+    }
+}
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeaponManager : NetworkBehaviour
+{
+    public const string WEAPON_LAYER = "Weapon";
+
+                        public  static  GameObject[]    msWeaponArr         = new GameObject[18];
+    [SerializeField]    public          PlayerWeapon    mPrimary;
+    [SerializeField]    public          PlayerWeapon    mSecondary;
+    [SerializeField]    private         PlayerWeapon    mSuper;
+    [SerializeField]    private         Transform       weaponHolder;
+    [SerializeField]    private         GameObject[]    mWeaponArr          = new GameObject[18];
+    [SerializeField]    private         CameraRecoil    mCameraRecoil;
+    [SerializeField]    private         ModelRecoil     mModelRecoil;
+                        private         PlayerWeapon    mCurrent;
+                        private         PlayerWeapon    mNextWeapon;
+                        private         WeaponGraphics  mCurrentGraphics;
+                        private         bool            mAmplified          = false;
+                        private         bool            mHasted             = false;
+                        private         float           mAmplifyTime        = 0f;
+                        private         float           mHasteTime          = 0f;
+                        private         float           AMPLIFY_DURATION    = 5f;
+                        private         float           HASTE_DURATION      = 5f;
+
+    private void Start()
+    {
+        for (int i = 0; i < mWeaponArr.Length; ++i)
+        {
+            if (mWeaponArr[i] != null && msWeaponArr[i] == null)
+            {
+                msWeaponArr[i] = mWeaponArr[i];
+            }
+        }
+
+        // Equip Each weapon.
+        mPrimary = new ShotRifle
+        {
+            readyToShoot = true
+        };
+
+        Equip(mPrimary);
+        mCurrent = mPrimary;
+        mCameraRecoil.UpdateRecoilInfo(mCurrent.cameraRecoilInfo);
+        mModelRecoil.UpdateRecoilInfo(mCurrent.modelRecoilInfo);
+        mCurrent.model.SetActive(true);
+
+        // Create Sidearm
+        mSecondary = new BurstPistol
+        {
+            readyToShoot = false
+        };
+        Equip(mSecondary);
+
+        mSuper      = new RocketLauncher
+        {
+            readyToShoot = false
+        };
+        Equip(mSuper);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Weapon Switching
+        if (Input.GetButtonDown("Primary") && !mCurrent.altFire && mPrimary != mCurrent && mPrimary != null)
+        { 
+            mNextWeapon = mPrimary;
+            mCurrent.reloading = false;
+            Invoke(nameof(Stow), mCurrent.stowTime);
+        }
+        if (Input.GetButtonDown("Secondary") && !mCurrent.altFire && mSecondary != mCurrent && mSecondary != null)
+        {
+            mNextWeapon = mSecondary;
+            mCurrent.reloading = false;
+            Invoke(nameof(Stow), mCurrent.stowTime);
+        }
+        if (Input.GetButtonDown("Super") && !mCurrent.altFire && mSuper != mCurrent && mSuper != null)
+        {
+            mNextWeapon = mSuper;
+            mCurrent.reloading = false;
+            Invoke(nameof(Stow), mCurrent.stowTime);
+        }
+
+        if (mAmplified)
+        {
+            mAmplifyTime    += Time.deltaTime;
+
+            if (mAmplifyTime > AMPLIFY_DURATION)
+            {
+                mAmplifyTime = 0f;
+
+                mAmplified = false;
+
+                mPrimary.amplified      = false;
+                mSecondary.amplified    = false;
+
+                if (mSuper != null)
+                {
+                    mSuper.amplified    = false;
+                }
+            }
+        }
+
+        if (mHasted)
+        {
+            mHasteTime      += Time.deltaTime;
+
+            if (mHasteTime > HASTE_DURATION)
+            {
+                mHasteTime = 0f;
+
+                mHasted = false;
+
+                mPrimary.hasted             = false;
+                mPrimary.speedMultiplier    = 1.5f; 
+                mSecondary.hasted           = false;
+                mSecondary.speedMultiplier  = 1.5f;
+
+                if (mSuper != null)
+                {
+                    mSuper.hasted           = false;
+                    mSuper.speedMultiplier  = 1.5f;
+                }
+            }
+        }
+    }
+
+    public PlayerWeapon GetCurrentWeapon() {
+        return mCurrent;
+    }
+
+    public WeaponGraphics GetCurrentWeaponGraphics() {
+        return mCurrentGraphics;
+    }
+
+    public ModelRecoil GetModelRecoil() {
+        return mWeaponArr[(int)mCurrent.weaponType].GetComponent<ModelRecoil>();
+    }
+
+    private void Equip(PlayerWeapon weapon)
+    {
+        weapon.model = (GameObject)Instantiate(
+            msWeaponArr[(int)weapon.weaponType]);
+        weapon.model.transform.SetParent(weaponHolder);
+        Vector3 holdPosition = weapon.model.GetComponentInChildren<HoldPt>().gameObject.transform.position;
+        weapon.model.transform.position = weaponHolder.transform.position - holdPosition;
+     
+
+        mCurrentGraphics = weapon.model.GetComponentInChildren<WeaponGraphics>();
+        if (mCurrentGraphics == null)
+        {
+            throw new Exception("No weapon graphics component on " + weapon.weaponType);
+        }
+
+        if (isLocalPlayer) {
+            Util.SetLayerRecursively(weapon.model, LayerMask.NameToLayer(WEAPON_LAYER));
+        }
+
+        // Hide model initially.
+         weapon.model.SetActive(false);
+    }
+
+    private void Stow()
+    {
+        mCurrent.readyToShoot = false;
+        Invoke(nameof(Draw), this.mCurrent.stowTime);
+
+        // Todo: Animate Stowing here
+        //
+        //
+    }
+
+    private void Draw() 
+    {
+        // Hide the model
+        mCurrent.model.SetActive(false);
+
+        // Switch active weapon
+        mCurrent = mNextWeapon;
+        mCurrent.model.SetActive(true);
+        mCameraRecoil.UpdateRecoilInfo(mCurrent.cameraRecoilInfo);
+        mModelRecoil.UpdateRecoilInfo(mCurrent.modelRecoilInfo);
+
+        // Todo: Trigger drawing animation here.
+        //
+        //
+
+        // Allow player to shoot after the drawing completes (alternatively can invoke via an animation event).
+        Invoke(nameof(ReadyToShoot), mCurrent.drawTime);
+    }
+
+    private void ReadyToShoot()
+    {
+        mCurrent.readyToShoot = true;
+    }
+
+
+    public void Respawn()
+    {
+        mPrimary.Reset();
+        mSecondary.Reset();
+        Destroy(mSuper.model);
+        mSuper                                                      = null;
+        mSecondary.model.SetActive(false);
+        mCurrent                                                    = mPrimary;
+        mCurrent.model.SetActive(true);
+        mCameraRecoil.UpdateRecoilInfo(mCurrent.cameraRecoilInfo);
+        mModelRecoil.UpdateRecoilInfo(mCurrent.modelRecoilInfo);
+        mCurrent.readyToShoot                                       = true;
+    }
+
+    public void AmplifyDamage()
+    {
+        if (mAmplified)
+        {
+            mAmplifyTime = 0f;
+        }
+        else
+        {
+            mPrimary.amplified      = true;
+            mSecondary.amplified    = true;
+            if (mSuper != null)
+            {
+                mSuper.amplified    = true;
+            }
+            mAmplified = true;
+        }
+    }
+
+    public void Haste()
+    {
+        if (mHasted)
+        {
+            mHasteTime = 0f;
+        }
+        else
+        {
+            mPrimary.hasted             = true;
+            mPrimary.speedMultiplier    = 1.5f;
+            mSecondary.hasted           = true;
+            mSecondary.speedMultiplier  = 1.5f;
+
+            if (mSuper != null)
+            {
+                mSuper.hasted           = true;
+                mSuper.speedMultiplier  = 1.5f;
+            }
+
+            mHasted = true;
+        }
+    }
+
+}
